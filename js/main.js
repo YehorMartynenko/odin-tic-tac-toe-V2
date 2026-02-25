8dee122027683a48dc1d28426556c6d5225deb05
YehorMartynenko <yehor.martynenko.27@gmail.com>
2026-02-25 13:02:14 +0100

    Add win condition

--- js/main.js
+++ js/main.js
@@ -24,28 +24,37 @@ const Gameboard = (function() {
 
     createBoard();
 
-    const printBoard = () => {
+    const getBoardSnapshot = () => {
         const boardWithCellValues = board.map((row) => row.map((cell) => cell.getValue()));
         console.table(boardWithCellValues);
-    };
-
-    const placeMark = (row, col, playerMark) => {
-        const isValidRow = (row >= 0 && row <= 3);
-        const isValidCol = (col >= 0 && col <= 3);
-
-        if(!(isValidRow && isValidCol)){
-            return false;
-        } else if(board[row][col].getValue() === ""){
-            board[row][col].addPlayerMark(playerMark)
-            return true;
-        } else {
-            return false;
-        }
-
-        
+        return boardWithCellValues;
     }
 
-    return { printBoard, placeMark };
+    function markManager() {
+        const placeMark = (row, col, playerMark) => {
+            const isValidRow = (row >= 0 && row <= 2);
+            const isValidCol = (col >= 0 && col <= 2);
+
+            if(!(isValidRow && isValidCol)){
+                return false;
+            } else if(board[row][col].getValue() === ""){
+                board[row][col].addPlayerMark(playerMark)
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        const getMark = (row, col) => {
+            return board[row][col].getValue();
+        }
+
+        return {placeMark, getMark};
+    }
+
+    const manager = markManager();
+
+    return { getBoardSnapshot, placeMark: manager.placeMark, getMark: manager.getMark, createBoard };
 })();
 
 const GameController = (function(playerOneName = "Player One", playerTwoName = "Player Two") {
@@ -65,21 +74,73 @@ const GameController = (function(playerOneName = "Player One", playerTwoName = "
         return activePlayer === players[0] ? players[1] : players[0];
     }
 
-    const getActivePlayer = () => {
-        return activePlayer;
-    }
 
     const playRound = (row, col) => {
         console.log(`${activePlayer.name}'s turn`);
         let isSucces = true;
         isSucces = Gameboard.placeMark(row, col, activePlayer.mark);
         if(!isSucces){
-            console.log(`Please make sure your coordinates are in [0,3] range and selected cell is empty`);
+            console.log(`Please make sure your coordinates are in [0,2] range and selected cell is empty`);
             return;
         }
-        Gameboard.printBoard();
+        Gameboard.getBoardSnapshot();
+        if(GameRules.checkWin(Gameboard.getBoardSnapshot())){
+            console.log(`${activePlayer.name} won!`);
+            activePlayer = players[0];
+            Gameboard.createBoard();
+            return;
+        }
         activePlayer = changeActivePlayer();
     }
 
-    return { playRound, getActivePlayer }
+    return { playRound }
+})();
+
+const GameRules = (function() {
+    const winConditionCoords = [];
+
+    const addRowsCoords = () => {
+        for(let row = 0; row<3; row++){
+            const coords = [];
+            for(let col = 0; col<3; col++){
+                coords.push([row, col]);
+            }
+            winConditionCoords.push(coords);
+       }
+    }
+
+    const addColsCoords = () => {
+        for(let row = 0; row<3; row++){
+            const coords = [];
+            for(let col = 0; col<3; col++){
+                coords.push([col, row]);
+            }
+            winConditionCoords.push(coords);
+       }
+    }
+
+    const addDiagCoords = () => {
+        const coords = [];
+        const coords2 = [];
+        for(let index = 0; index<3; index++){
+            coords.push([index, index]);
+        }
+        winConditionCoords.push(coords);
+        for(let index = 0; index<3; index++){
+            coords2.push([index, 2 - index]);
+        }
+        winConditionCoords.push(coords2);
+        
+    }
+
+    addRowsCoords();
+    addColsCoords();
+    addDiagCoords();
+
+    const checkWin = (board) => {
+        const lines = winConditionCoords.map((item) => item.map((el) => board[el[0]][el[1]]));
+        const isWin = lines.map((line) => line.every((mark, index, arr) => mark === arr[0] && mark !== "")).filter((el) => el === true);
+        return isWin.length === 0 ? false : true;
+    }
+    return {checkWin}
 })();
